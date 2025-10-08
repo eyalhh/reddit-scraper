716d4471ee2e7f04d07e8ed214b8e0effec305b5
eyal <eyalz2007@gmail.com>
2025-10-08 19:29:23 +0300

    adding pulling func to get to desired numposts

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"github.com/eyalhh/reddit-scraper/scraper"
+	"encoding/json"
 	"log"
-	"github.com/joho/godotenv"
 	"os"
+	"fmt"
+	"github.com/eyalhh/reddit-scraper/scraper"
+	"github.com/joho/godotenv"
+	"strconv"
 )
 
 
 
 func main() {
-	err := godotenv.Load()
+	
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s <subreddit_name> <num_posts>\n", os.Args[0])
+		return
+	}
+	subreddit := os.Args[1]
+	numPosts, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("second argument must be integer\n")
+		return
+	}
+
+	err = godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -20,8 +35,9 @@ func main() {
 	data := scraper.RequestData{
 		OperationName: "SubredditFeedElements",
 		Variables: scraper.RequestVariables{
-			SubredditName: "funny",
+			SubredditName: subreddit,
 			Sort: "HOT",
+			Range: "ALL",
 			IncludeSubredditInPosts: false,
 			IncludePostStats: true,
 			IncludeCurrentUserAwards: true,
@@ -34,5 +50,15 @@ func main() {
 		},
 	}
 
-	scraper.GetPosts(data, authKey)
+	posts, err := scraper.GetPostsAtLength(data, authKey, numPosts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonPosts, err := json.MarshalIndent(posts, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("the posts are:\n" + string(jsonPosts))
+
 }
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,11 +1,12 @@
 package scraper
 
 import (
-	"net/http"
-	"encoding/json"
-	"fmt"
 	"bytes"
+	"encoding/json"
+	"encoding/base64"
+	"fmt"
 	"io"
+	"net/http"
 )
 
 const (
@@ -20,6 +21,8 @@ type RequestData struct {
 type RequestVariables struct {
 	SubredditName            string `json:"subredditName"`
 	Sort                     string `json:"sort"`
+	Range                    string `json:"range"`
+	After                    string `json:"after"`
 	IncludeSubredditInPosts  bool   `json:"includeSubredditInPosts"`
 	IncludePostStats         bool   `json:"includePostStats"`
 	IncludeCurrentUserAwards bool   `json:"includeCurrentUserAwards"`
@@ -34,37 +37,116 @@ type PersistedQuery struct {
 	Sha256Hash string `json:"sha256Hash"`
 }
 
-func GetPosts(data RequestData, token string) error {
+type ResponseData struct {
+	Data InnerResponseData `json:"data"`
+}
+
+type InnerResponseData struct {
+	PostFeed PostFeed `json:"postFeed"`
+}
+
+type PostFeed struct {
+	Elements ResponseElements `json:"elements"`
+}
+
+type ResponseElements struct {
+	Edges []ResponseEdges `json:"edges"`
+}
+
+type ResponseEdges struct {
+	Node EdgeNode `json:"node"`
+}
+
+type EdgeNode struct {
+	ID           string     `json:"id"`
+	CreatedAt    string     `json:"createdAt"`
+	Title        string     `json:"title"`
+	CommentCount int        `json:"commentCount"`
+	AuthorInfo   AuthorInfo `json:"authorInfo"`
+}
+
+type AuthorInfo struct {
+	Name string `json:"name"`
+}
+
+type Post struct {
+	ID           string `json:"id"`
+	Title        string `json:"title"`
+	AuthorName   string `json:"authorName"`
+	CommentCount int    `json:"commentCount"`
+	CreatedAt    string `json:"createdAt"`
+}
+
+func GetPostsAtLength(data RequestData, token string, length int) ([]Post, error) {
+
+	var currentLen int
+	var res []Post
+
+	for currentLen < length {
+		posts, err := GetPosts(data, token, "")
+		if err != nil {
+			return nil, err
+		}
+		if len(posts) == 0 {
+			return res, fmt.Errorf("not enough posts: %d/%d", len(res), length)
+		}
+		data.Variables.After = base64.StdEncoding.EncodeToString([]byte(posts[len(posts)-1].ID))
+		res = append(res, posts...)
+		currentLen += len(posts)
+	}
+
+	return res, nil
+
+}
+
+func GetPosts(data RequestData, token string, afterId string) ([]Post, error) {
 
 	jsonData, err := json.MarshalIndent(data, "", "    ")
 	if err != nil {
-		return err
+		return nil, err
 	}
-	fmt.Println(string(jsonData))
 
 	req, err := http.NewRequest("POST", BASE_URL, bytes.NewBuffer(jsonData))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	req.Header.Set("Authorization", "Bearer " + token)
+	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	fmt.Println("Respnonse body:\n", string(respBody))
+	var responseData ResponseData
+	if err := json.Unmarshal(respBody, &responseData); err != nil {
+		return nil, err
+	}
 
-
-	return nil	
+	return formatPosts(responseData), nil
 }
 
+func formatPosts(responseData ResponseData) []Post {
+
+	var posts []Post
+	for _, post := range responseData.Data.PostFeed.Elements.Edges {
+		var newPost Post
+		newPost.ID = post.Node.ID
+		newPost.CreatedAt = post.Node.CreatedAt
+		newPost.Title = post.Node.Title
+		newPost.CommentCount = post.Node.CommentCount
+		newPost.AuthorName = post.Node.AuthorInfo.Name
+		posts = append(posts, newPost)
+	}
+
+	return posts
+
+}
